c12f26ec9e31110cc75c8b799414b932a2091242
harisnaseer24 <harisnaseer258@gmail.com>
2024-10-18 17:57:05 +0500

    Intro to JS and variables with datatypes

---
+++ 20th Class JS/app.js
@@ -0,0 +1,46 @@
+//variables  -- a container to store some values in memory.
+//keyword -- some reserve words which have special meanings
+//kw    variable-name           value
+// let    firstName       =    "Haris"; //Camel case
+// let age = 45 ;
+
+// alert(firstName);
+
+// // rules:
+// // 1. number or other special characters cannot be used in start of the name.
+// // let 1stname="dfdf";
+// // let .dkfd="dfgdfd";
+
+// // 2. no space in variable name.
+// // let dk fd="dfgdfd";
+// // 3. Use Camel Case. fullName
+// let fullName ="Haris Naseer";
+// //4. _ or $ are allowed to start variable name.
+// let $df="dfgdfd";
+// let _dkfdfdd="dfgdfd";
+
+// // Datatypes
+// // primitive types(basic)
+
+// let name="24";//string--
+// let marks=334340.38947549754;//number --
+// let isRed=false;//boolean --
+
+let number1=24;
+let number2=2;
+//Operators
+//Arithmetic operators
+
+// alert(number1 + number2);//Add
+// alert(number1 - number2);//Sub
+// alert(number1 * number2);//48//Multiply
+// alert(number1 / number2);//12 //divide
+// alert(number1 % number2);//24%2=0 //Modulus
+
+let email="haris@gmail.com";
+let greeting="Good Evening!,";
+// alert(greeting +" from "+ email);//Concatenation
+console.log(56 + "haris")//56haris
+// 24 + 2 = 26
+console.log(number1 + " + " + number2 +" = "+ (number1+number2))//56haris  
+//32 - 4 + 66 = 38
