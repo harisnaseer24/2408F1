22d1e1e828f2b7dd1c711b43803a58b3fe2f3677
Haris Naseer <harisnaseer@aptech.com>
2024-10-22 19:17:24 +0500

    conditional statements (if-else ladder and nested ifs)

---
+++ 22th class/app.js
@@ -0,0 +1,53 @@
+// let age = +prompt("Enter your age :");
+// console.log(age);
+
+//NaN // Not a number
+
+
+
+    //if else ladder
+    // if (age >18 && age<=70) { // 19 - 70
+    //     console.log("you can drive")
+    
+    // }
+    // else if(age == 18){
+    
+    //     console.log("you can drive, but make sure you have liscense.")
+    // }
+    // else if(age > 110 || age < 1){
+    //     console.log("invalid age")
+    // }
+    
+    // else {
+    //     console.log("you cannot drive")
+    // }    
+
+
+
+//Nested ifs
+    let areYouHungry=prompt("Are you hungry? Yes or no:").toLowerCase();// YES  - > yes
+alert(areYouHungry);
+
+    if(areYouHungry == "yes"){
+
+        let food=prompt("Kia khaoge..?").toUpperCase();//biryani -> BIRYANI
+        if(food =="BIRYANI"){
+            console.log("15 mint m biryani ajai gi.")
+        }else{
+            console.log("10 mint m "+food+" ajai ga.")
+        }
+
+    }else{
+        console.log("That's good, Thanks.");
+    }
+
+//.1 calculator
+
+// if(op == "+"){
+//     alert(num1+num2)
+// }else{
+
+// }
+
+//.2. Are you  coder?  
+    
