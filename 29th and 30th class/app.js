b734c57ce4125573e1d37407d25af371d1a00af0
harisnaseer24 <harisnaseer258@gmail.com>
2024-11-16 19:02:28 +0500

    login done

--- 29th and 30th class/app.js
+++ 29th and 30th class/app.js
@@ -19,7 +19,35 @@ users.push(user);
 let jsonUser= JSON.stringify(users);
 // console.log(users[0]);
 localStorage.setItem("users",jsonUser);
-alert("Registeration Success");
+alert("Registeration Success. Please Login now..!");
+
+window.location.href="login.html";
 
 
+}
+// arrow function
+
+let Login= ()=>{
+
+    let users= JSON.parse(localStorage.getItem("users"));
+
+    if(users.length !=null){
+        let uemail= document.getElementById("email").value;
+        let upassword= document.getElementById("password").value;
+     
+        let checkuser=users.filter((user)=>{
+           if(user.email == uemail && user.password == upassword){
+    return user;
+           }
+        })
+        if (checkuser[0]!= null) {
+            alert("Login success");
+            window.location.href = "./home.html";
+        } else {
+            alert("Invalid credentials");
+        }
+    }
+    else{
+        alert("User not found");
+    }
 }
